a7ce168e070f2989845cd74618044af1cea44572
a <isai@ihitoshitokushinoMacBook-Air.local>
2018-09-12 15:34:32 +0900

    管理者権限追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,7 +3,15 @@ module SessionsHelper
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+  def admin_user
+    @admin_user ||= User.where("name = 'admin'")
+  end
+
   def logged_in?
     current_user.present?
+  end
+
+  def admin_logged_in?
+    admin_user.present?
   end
 end
